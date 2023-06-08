69b009eded1cbb188525957e8a9b2ad75c10de0e
Osher <Osher8704@gmail.com>
2023-06-08 10:17:29 +0300

    base proj

---
+++ ProductsAuthomation/ProductsAuthomation/Tests/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+
+namespace ViteReactTS
+{
+    [TestClass]
+    public class BaseTest
+
+    {
+        public ChromeDriver driver { get; set; }
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            driver = new ChromeDriver(@"C:\QA\ViteReactTS\ViteReactTS\ViteReactTS\ViteReactTS\chromedriver\chromedriver.exe");
+            driver.Navigate().GoToUrl(Pages.BasePage.ViteReactST_URL);
+            driver.Manage().Window.Maximize();
+        }
+        [TestCleanup]
+        public void FinishTest()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ ProductsAuthomation/ProductsAuthomation/Tests/HomeTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViteReactTS.Pages;
+
+namespace ViteReactTS.Tests
+{
+    [TestClass]
+    public class HomeTest : BaseTest
+    {
+        [TestMethod]
+        public void Home()
+        {
+            HomePage Home = new HomePage(driver);
+        }
+    }
+}
